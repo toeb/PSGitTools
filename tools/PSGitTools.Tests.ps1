7aa82312042873c12dbdc3542c45e3ff9dd51b94
Tobias Becker <becker.tobi@gmail.com>
2014-01-04 23:06:02 +0100

    added tests

---
+++ tools/PSGitTools.Tests.ps1
@@ -0,0 +1,213 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "PSGitTools" {
+    pushd
+    
+    mkdir "$TestDrive/myRepo"
+    mkdir "$TestDrive/myRepo2.git"
+    git init "$TestDrive/myRepo"
+    git init "$TestDrive/myRepo2.git"
+    
+    Context "Get-GitRepositoryName"{
+        
+        It "should parent directory name if remote is  not set" {
+           mkdir "$TestDrive/repo1"
+
+           $result = Get-GitRepositoryName "$TestDrive/repo1";
+           $result | Should be "repo1"
+        }
+
+        It "should get the remote name if it is a remote repository" {
+
+            git clone "$TestDrive/myRepo" "$TestDrive/otherDir"
+
+            $result = Get-GitRepositoryName "$TestDrive/otherDir"
+            $result | Should Be "myRepo"
+        }
+
+        It "should remove '.git' extension from name"{
+            git clone "$TestDrive/myRepo2.git" "$TestDrive/otherDir2"
+            $result = Get-GitRepositoryName "$TestDrive/otherDir2"
+            $result | Should Be "myRepo2"
+        }
+    }
+
+
+    Context "Get-Revision" {
+
+        It "Should return 0 for a newly created repository"{
+            mkdir "$TestDrive/repo3"
+            cd "$TestDrive/repo3"
+            git init
+            $result = Get-Revision
+            $result | Should Be 0
+        }
+
+        It "Should return the correct revision number"{
+            mkdir "$TestDrive/repo4"
+            cd "$TestDrive/repo4"
+            git init
+            "some text" > "readme.md"
+            git add "readme.md"
+            git commit -m "message" 
+            $result = Get-Revision "$TestDrive/repo4"
+            $result | Should Be 1
+        }
+
+        It "Shoudl return the correct revision number after 2 commits"{
+
+            mkdir "$TestDrive/repo5"
+            cd "$TestDrive/repo5"
+            git init
+            "some text" > "readme.md"
+            git add .
+            git commit -m "message"
+            "some new text" > "readme.md"
+            git add -u
+            git commit -m "message2"
+            $result = Get-Revision
+            $result | should be 2
+        }
+    }
+
+    Context "TagCurrentVersion" {
+        It "Should create v0.0.0.1 tag for initial version" {
+            #arrange
+            mkdir "$TestDrive/repo7" | cd
+            git init > $null
+            "text" > readme.md
+            git add .
+            git commit -m "initial commit"
+            
+            #act
+            $version = TagCurrentVersion "$TestDrive/repo7"
+
+            # check
+            "$version" | should be "0.0.0.1"
+            "$(get-lastversion)" | Should be "0.0.0.1"
+        }
+
+        It "Should no fail if current version is already tagged" {
+            #arrange
+            mkdir "$TestDrive/repo9" | cd
+            git init 
+            "text" > readme.md
+            git add .
+            git commit -m "init"
+
+            #act
+            $version1 = TagCurrentVersion "m"
+            $version2 = TagCurrentVersion "m2"
+
+            # check
+            "$version2" | should be "$version1"
+
+        }
+
+        
+    }
+
+    Context "TagNextMinorVersion"{
+        It "should tag version 0.1" {
+            #arrange
+            mkdir "$TestDrive/repo9" | cd
+            git init 
+            "text" > readme.md
+            git add .
+            git commit -m "init"
+
+            $version = TagNextMinorVersion
+
+            "$version" | should be "0.1.0.1"
+        }
+        
+
+    }
+    Context "TagNextMajorVersion"{
+        It "should tag version 1.0"{
+            
+            #arrange
+            mkdir "$TestDrive/repo9" | cd
+            git init 
+            "text" > readme.md
+            git add .
+            git commit -m "init"
+
+            $version = TagNextMajorVersion
+
+            "$version" | should be "1.0.0.1"
+
+        }
+
+        It "should reset minor version to 0"{
+           #arrange
+            mkdir "$TestDrive/repo9" | cd
+            git init 
+            "text" > readme.md
+            git add .
+            git commit -m "init"
+
+            $version = TagNextMinorVersion "message"
+            $version = TagNextMajorVersion "message"
+
+            "$version" | should be "1.0.0.1"
+
+
+        }
+
+        It "should increment major version"{
+           #arrange
+            mkdir "$TestDrive/repo9" | cd
+            git init 
+            "text" > readme.md
+            git add .
+            git commit -m "init"
+
+            $version = TagNextMajorVersion "message"
+            $version = TagNextMajorVersion "message"
+
+            "$version" | should be "2.0.0.1"
+
+        }
+
+    }
+
+
+    Context "PushLastTag" {
+        It "should push last tag to remote"{
+            
+        }
+    }
+
+
+    Context "Get-LastVersion" {
+        It "Should return 0.0 if no version is present"{
+            #arrange
+            mkdir "$TestDrive/repo8" | cd
+
+            # act
+            "$(Get-LastVersion)" | should be "0.0"
+            
+        }
+    }
+
+    Context "Get-CurrentVersion"    {
+        It "Should return 0.0.0.0 if no version is present"{
+            #arrange
+            mkdir "$TestDrive/repo6" |cd
+
+            # act
+            "$(Get-CurrentVersion)" | should be "0.0.0.0"
+            
+        } 
+
+        
+
+    }
+
+
+    popd
+}
+
--- tools/PSGitTools.ps1
+++ tools/PSGitTools.ps1
@@ -2,6 +2,7 @@
 #assumes an initial tag is present
 # assumes tags are named v{major}.{minor}.[{revision}]
 function Get-LastVersion(){
+[OutputType([version])]
 $lastTagCommit = git rev-list --tags --max-count=1
 $lastTag = git describe --tags $lastTagCommit
 $tagPrefix = "v"
@@ -10,19 +11,50 @@ write-host -NoNewline "last tagged commit "
 write-host -NoNewline -ForegroundColor "yellow" $lastTag
 write-host -NoNewline " revision "
 write-host -ForegroundColor "yellow" "$lastTagCommit"
-[reflection.assembly]::LoadWithPartialName("System.Version")
 
-$version = New-Object System.Version($versionString)
+[version]$version = New-Object System.Version($versionString)
 return $version;
-
+}
+#returns the name of the repository 
+#first checks remote name, then uses name of containing directory
+function Get-GitRepositoryName($path){
+    if($path -eq $Null){
+        $path = Get-Location
+    }
+    pushd
+    cd $path
+    $result= git config --get remote.origin.url
+    if($result -eq $Null){
+        $result = Get-Location
+    } 
+    [string] $name = Split-Path $result -Leaf
+    if($name.EndsWith(".git")){
+        $name=$name.Substring(0,$name.Length-4)
+    }
+    popd
+    return $name;
 }
 
-# returns current revision by counting the nummer of commits to HEAD
-function Get-Revision(){
-$lastTagCommit = git rev-list HEAD
- $revs  = git rev-list $lastTagCommit |  Measure-Object -Line 
- return $revs.Lines
+
+
+# returns current revision by counting the nummer of commits
+function Get-Revision($path){
+    if($path -eq $Null) {
+        $path = Get-Location
+    }
+    pushd
+    cd $path
+    $lastTagCommitResult = ExecGit "rev-list HEAD" 
+
+    $lastTagCommitResultLines= $lastTagCommitResult | Split-String -RemoveEmptyStrings
+
+    $lastTagCommit = $lastTagCommitResultLines |  select -First 1 -Last 1
+    $revs  = ExecGit " rev-list $lastTagCommit  |  Measure-Object -Line"
+    popd
+    return $revs.Lines
 }
+
+
 #returns the next major version {major}.{minor}.{revision}
 function Get-NextMajorVersion(){
     $version = Get-LastVersion;
@@ -43,12 +75,17 @@ function Get-NextMinorVersion(){
     return $next;
 }
 
+function Get-BuildNumber(){
+    return 0;
+}
+
 #returns the last minor version with an up to date revision number
 function Get-CurrentVersion(){
-    $version = Get-LastVersion;
-    [reflection.assembly]::LoadWithPartialName("System.Version")
+    [OutputType([version])]
+    [version]$version = Get-LastVersion;
     $rev = Get-Revision;
-    $next = New-Object System.Version($version.Major, $version.Minor, $rev);
+    $build = Get-BuildNumber;
+    [version]$next = New-Object System.Version($version.Major, $version.Minor,$build, $rev);
     return $next;
 }
 
@@ -60,14 +97,33 @@ function TagNextMinorVersion($tagMessage){
     write-host -ForegroundColor DarkYellow "$tagName"; 
     git tag -a $tagName -m $tagMessage
 }
-function TagCurrentRevision($tagMessage){
-   $version = Get-CurrentVersion;
-   $tagName = "v{0}" -f "$version".Trim();
+
+# creates a tag for the the current version
+# the current version consists of the manually set major and minor version
+# and the curretn revision.
+function TagCurrentVersion{
+    param(
+     [Parameter(Mandatory=$true,HelpMessage="commit message for current tag")] [string] $tagMessage,
+     [Parameter(HelpMessage="the root directory of the local repository, if not specified the current directory is used")]$path
+    )
+
+    if($path -eq $null){
+        $path = Get-Location
+    }
+    pushd 
+    cd $path
+    $version = Get-CurrentVersion;
+    $tagName = "v{0}" -f "$version".Trim();
     write-host -NoNewline "Tagging current revision to ";
     write-host -ForegroundColor DarkYellow "$tagName"; 
     
     git tag -a $tagName -m $tagMessage
-   
+    popd
+    return $version
+}
+function Get-CurrentTagCommitMessage($path){
+    $version = Get-CurrentVersion $path
+    return "Tagging version $version"
 }
 #creates a tag with the next major version (minor version starts again at 0)
 function TagNextMajorVersion($tagMessage){
@@ -76,14 +132,35 @@ function TagNextMajorVersion($tagMessage){
     write-host -NoNewline "Tagging next majo version to ";
     write-host -ForegroundColor DarkYellow "$tagName"; 
     git tag -a $tagName -m $tagMessage
+    return $version
 }
 
+# gets the last tagged version and pushes it to the remote
 function PushLastTag(){
- $version = Get-LastVersion;  
-
- $tagName=  "v{0}" -f "$version".Trim();
- $ErrorActionPreference='Stop';
-
+    $version = Get-LastVersion;  
+    $tagName=  "v{0}" -f "$version".Trim();
+    $ErrorActionPreference='Stop';
     git push origin $tagName
-    
+    return $version;
+}
+
+
+function Exec($file, $arguments){
+$processStartInfo = new-object system.diagnostics.processStartInfo
+$processStartInfo.workingDirectory = (get-location).path
+$processStartInfo.fileName = $file
+$processStartInfo.arguments = $arguments
+$processStartInfo.useShellExecute = $false
+$processStartInfo.RedirectStandardOutput = $true;
+$processStartInfo.RedirectStandardError = $true;
+$process = [system.diagnostics.process]::start($processStartInfo);
+$result =  $process.waitForExit();
+return $process.StandardOutput.ReadToEnd();
+
+}
+
+# silently executes a git  string
+function ExecGit($arguments){
+ $result=  Exec -file "git" -arguments $arguments
+ return $result;
 }
